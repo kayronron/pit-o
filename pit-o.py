f5dc39167ea0b71c9dc82f156b8c6a5adae2ac11
kayronron <amorim.kayron@escola.pr.gov.br>
2025-03-24 08:47:20 -0300

    atualização do codigo em python

---
+++ pit-o.py
@@ -0,0 +1,9 @@
+idade = int(input("Qual sua idade? "))
+if idade < 16:
+   print("Não precisa votar")
+elif(idade == 16 or idade >65)
+   print("Voto não obrigatorio");
+else
+   print("Voto obrigatorio");
+
+
