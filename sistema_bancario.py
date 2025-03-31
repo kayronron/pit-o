d1a9341a04325281bb48f88d4fb2cdcca36f1f13
kayronron <amorim.kayron@escola.pr.gov.br>
2025-03-31 07:49:36 -0300

    jgfvjf ç

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,53 +1,55 @@
-#print("********Menu********")
-#print("Criar usuário")
+#print("**********Menu**********")
+#print("Criar Usuário")
 #print("Depositar")
 #print("Sacar")
 
 saldo = 0
 limite = 500
 numero_saques = 0
-limite_saque = 3
-usuario = []
+limites_saques = 3
+usuarios =[]#criei uma lista vazia , pois serão adicionados valores posteriormente
 contas = []
-agencia = '001'
+agencia='0001'
 
 
 def menu():
     print('''
-    ********Menu********
-    (1) Criar usuário
-    (2) Criar conta
-    (3) Listar contas
-    (d) Depositar
-    (s) Sacar
-    (e) Extrato
-    (q) Sair
+        **********Menu**********
+        (1) Criar Usuário
+        (2) Criar Conta
+        (3) Listar Contas
+        (d) Depositar
+        (s) Sacar
+        (e) Extrato
+        (q) Sair
     ''')
     return input('Qual opção deseja')
 
 def deposito(valor,saldo):
-
+    #  vou receber o valor do depósito e somar ao saldo
     if valor > 0 :
         saldo += valor
-
-        print(f'Você depositou R${valor}')
+        #print mostrando o valor depositado
+        print(f'Você depositou R${valor}')      
     else:
-        print('Valor do deposito inválido. Verifique a quatia digitada.')
+        print('Valor do deposito Ínválido.Verifique a quantia digitada.')
+    
     return saldo
 
 def saque(saque,saldo):
-    global numero_saques,limite_saque
-    #de acordo com a documentação python, variaveis globais não ficam presas ao limite do escopo da função.
-    if numero_saques >= limite_saque
-     print('Você atingiu o limite de saque se dua conta. Tente novamente no proximo dia útil')
-    else:
+   global  numero_saques,limites,limites_saques
+    #De acordo com a documentação Python , váriaveis globais não ficam presas ao limite do escopo da função.
+   if numero_saques >= limites_saques:
+    print('Você atingiu o limite de saques de sua conta.Tente novamente no proximo dia útil')
+   else:
         if saque <= 0:
-            print("Saque inválido. Verifique o valor e tente novamente.")
+            print('Saque Inválido.Verifique o valor e tente novamente.')
         elif saque > limite :
-            print("Valor limite excedido. Verifique o valor e tente novamente.")
+            print('Valor limite excedido.Verifique o valor e tente novamente.')
         elif saque > saldo :
-            print("Saldo insulficiênte. Verifique o valor e tente novamente.")
+            print('Saldo insuficiênte.Verifique o valor e tente novamente.')
         else:
             saldo -= saque
             numero_saques += 1
-    return saldo
+            
+        return saldo        
